84c4fecc64e2d278bbf34dc1b2375a859daf43e8
Britt Carlton <brittanyrc95@gmail.com>
2022-08-09 16:36:27 -0400

    Implement checkPassword with bcrypt compareSynce

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,30 @@ router.post('/', (req, res) => {
         })
 });
 
+router.post('/login', (req, res) => {
+    // expects {email: 'emailString', password: 'passwordString'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+        
+        // verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    })
+})
+
 // PUT /api/users/:id
 router.put('/:id', (req, res) => {
     // expects {username: 'usernameString', email: 'email@string.com', password: 'passwordString'}
