6ba4ca1c6c8b6d79958957a9c27972479676fa24
Britt Carlton <brittanyrc95@gmail.com>
2022-08-15 15:00:42 -0400

    Add check for session to comment post route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -20,16 +20,19 @@ router.get('/', (req, res) => {
 
 // POST a new comment
 router.post('/', (req, res) => {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            // use the id from session
+            user_id: req.session.user_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
 });
 
 // DELETE a comment
