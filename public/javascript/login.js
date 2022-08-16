474637ac9599e6a378880a3f9cf45788f65e7d91
Britt Carlton <brittanyrc95@gmail.com>
2022-08-16 11:10:02 -0400

    Create dashboard handlebars template

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -53,7 +53,7 @@ async function loginFormHandler(event) {
 
         if (response.ok) {
             console.log('success');
-            document.location.replace('/');
+            document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
         }
